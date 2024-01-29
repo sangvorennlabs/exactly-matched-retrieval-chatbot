dea90e21b538fb0a5f793e88a6a9b159b86055a9
sangvo-rennlabs <155945208+sangvorennlabs@users.noreply.github.com>
2024-01-29 21:47:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from utils import *
-from io import StringIO, BytesIO
 from tempfile import NamedTemporaryFile
 import os
 from openai import OpenAI
@@ -20,12 +19,15 @@ if uploaded_file is not None:
         with NamedTemporaryFile(delete=False) as tmp:  
             tmp.write(bytes_data)     
             st.session_state['documents'] = load_pdf_file(tmp.name, chunk_overlap)      
-        os.remove(tmp.name)   
+        os.remove(tmp.name) 
+        print('loading uploaded_file - done')  
 
     exact_match_key_phrases = st.text_area(label='Exactly matched phrases (seperated by ending line)', height=100)
     exact_match_key_phrases = [x for x in exact_match_key_phrases.split('\n') if x !='']
     instruction = st.text_area(label='Instruction', height=50)
     if st.button('Ask GPT'):
+        print(f"main document length: {len(st.session_state['documents'])}")
+        print(st.session_state['documents'])
         document_list_result = [document for document in st.session_state['documents'] if any([x in document.page_content for x in exact_match_key_phrases])]
         context = "\n-------\n".join([document.page_content for document in document_list_result])
         prompt = f'You have to do the task describe in the instruction, base on the information in context:\n\ninstruction:\n[start ninstruction]\n{instruction}\n[end ninstruction]\n\n\context:\n[start context]\n{context}\n[end context]\n'
@@ -42,7 +44,11 @@ if uploaded_file is not None:
             model="gpt-3.5-turbo-1106",
             temperature=0
         )
+        print(chat_completion.choices[0].message.content)
         st.text_area(label="Answer", value=chat_completion.choices[0].message.content, height= 300)
         st.write([document.page_content for document in document_list_result])
+else:
+    st.session_state.clear()
+    print('clear')
     
 
